4eb2a835042f44dccdfcab93cb737e10c56f0e04
Jadir Xavier <49170436+JadirXavier@users.noreply.github.com>
2024-09-18 15:24:15 +0000

    Debug true

--- djangoapp/djangoapp/settings.py
+++ djangoapp/djangoapp/settings.py
@@ -47,7 +47,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
